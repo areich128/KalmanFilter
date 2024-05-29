73e7e8cebc098257089e8a285a5ea0b091018fc2
areich128 <alex.reich128@gmail.com>
2024-05-29 14:23:36 -0700

    more tweaks

--- importserial.py
+++ importserial.py
@@ -3,15 +3,14 @@ import serial
 # Set up the serial connection (adjust 'COM3' to your Arduino's port)
 ser = serial.Serial('COM3', 9600, timeout=1)
 
-# Open a text file for writing
 with open('output.txt', 'w') as file:
     while True:
         try:
             line = ser.readline().decode('utf-8').rstrip()
             if line:
-                print(line)  # Optional: Print the line to console
+                print(line)
                 file.write(line + '\n')
-                file.flush()  # Ensure data is written to the file
+                file.flush()
         except KeyboardInterrupt:
             print("Stopped by user")
             break
